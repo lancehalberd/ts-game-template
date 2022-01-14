9287b3a929bfc497d70f4bc6283b6a599b7d18ec
Chris Brewer <chris.brewer@adroll.com>
2022-01-14 12:01:07 -0600

    Fix whitespace.

--- app/client.ts
+++ app/client.ts
@@ -1,5 +1,6 @@
 import { CANVAS_WIDTH, CANVAS_HEIGHT, FRAME_LENGTH } from 'app/gameConstants';
-import { areAllImagesLoaded, requireImage } from 'app/utils/images';
+import { createAnimation, drawFrame, getFrame } from 'app/utils/animations';
+import { areAllImagesLoaded } from 'app/utils/images';
 import { bindKeyboardListeners,isKeyboardKeyDown, KEY } from 'app/utils/keyboard';
 
 
@@ -15,60 +16,103 @@ type Direction = 'up' | 'down' | 'left' | 'right';
 
 // Object to store the game state on.
 const state = {
-	time: 0,
-	lastRender: -1,
-	monkey: {
-		x: CANVAS_WIDTH / 2,
-		y: CANVAS_HEIGHT / 2,
-		direction: 'down' as Direction,
-	}
+    time: 0,
+    lastRender: -1,
+    monkey: {
+        x: CANVAS_WIDTH / 2,
+        y: CANVAS_HEIGHT / 2,
+        direction: 'down' as Direction,
+        animationTime: 0,
+        isWalking: false,
+    }
 };
 
 // 18x26, 4 frames: down - right - up - left
-const monkeyImage = requireImage('gfx/monkey/facing.png');
+const [standDown, standRight, standUp, standLeft] = createAnimation('gfx/monkey/facing.png', {w: 18, h: 26}, { cols: 4}).frames;
+const walkingGeometry = {w: 20, h: 28};
+const walkUpAnimation = createAnimation('gfx/monkey/mcwalking.png', walkingGeometry, { cols: 8, y: 2, duration: 4});
+const walkDownAnimation = createAnimation('gfx/monkey/mcwalking.png', walkingGeometry, { cols: 8, y: 0, duration: 4});
+const walkLeftAnimation = createAnimation('gfx/monkey/mcwalking.png', walkingGeometry, { cols: 8, y: 3, duration: 4});
+const walkRightAnimation = createAnimation('gfx/monkey/mcwalking.png', walkingGeometry, { cols: 8, y: 1, duration: 4});
+function getMonkeyFrame(monkey: typeof state.monkey) {
+    if (!monkey.isWalking) {
+        return {
+            up: standUp,
+            down: standDown,
+            left: standLeft,
+            right: standRight,
+        }[monkey.direction];
+    }
+    let animation = {
+        up: walkUpAnimation,
+        down: walkDownAnimation,
+        left: walkLeftAnimation,
+        right: walkRightAnimation,
+    }[monkey.direction]!;
+    return getFrame(animation, monkey.animationTime);
+}
 
 // Update function advances the state of the game every FRAME_LENGTH milliseconds.
 function update() {
-	state.time += FRAME_LENGTH;
-	if (isKeyboardKeyDown(KEY.UP)) {
-		state.monkey.y--;
-		state.monkey.direction = 'up';
-	}
-	if (isKeyboardKeyDown(KEY.DOWN)) {
-		state.monkey.y++;
-		state.monkey.direction = 'down';
-	}
-	if (isKeyboardKeyDown(KEY.LEFT)) {
-		state.monkey.x--;
-		state.monkey.direction = 'left';
-	}
-	if (isKeyboardKeyDown(KEY.RIGHT)) {
-		state.monkey.x++;
-		state.monkey.direction = 'right';
-	}
+    state.time += FRAME_LENGTH;
+    state.monkey.animationTime += FRAME_LENGTH;
+    if (isKeyboardKeyDown(KEY.UP)) {
+        state.monkey.direction = 'up';
+        state.monkey.y--;
+        if (!state.monkey.isWalking) {
+            state.monkey.isWalking = true;
+            state.monkey.animationTime = 0;
+        }
+    } else if (isKeyboardKeyDown(KEY.DOWN)) {
+        state.monkey.y++;
+        state.monkey.direction = 'down';
+        if (!state.monkey.isWalking) {
+            state.monkey.isWalking = true;
+            state.monkey.animationTime = 0;
+        }
+    } else if (isKeyboardKeyDown(KEY.LEFT)) {
+        state.monkey.x--;
+        state.monkey.direction = 'left';
+        if (!state.monkey.isWalking) {
+            state.monkey.isWalking = true;
+            state.monkey.animationTime = 0;
+        }
+    } else if (isKeyboardKeyDown(KEY.RIGHT)) {
+        state.monkey.x++;
+        state.monkey.direction = 'right';
+        if (!state.monkey.isWalking) {
+            state.monkey.isWalking = true;
+            state.monkey.animationTime = 0;
+        }
+    } else {
+        state.monkey.isWalking = false;
+    }
 }
 
 // Render will display the current state of the game.
 function render() {
-	if (!areAllImagesLoaded()) {
-		return;
-	}
-	// Only render if the state has been updated since the last render.
-	if (state.lastRender >= state.time) {
-		return;
-	}
-	state.lastRender = state.time;
+    if (!areAllImagesLoaded()) {
+        return;
+    }
+    // Only render if the state has been updated since the last render.
+    if (state.lastRender >= state.time) {
+        return;
+    }
+    state.lastRender = state.time;
 
-	// Draw an off-white background:
-	mainContext.fillStyle = '#EEE';
-	mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Draw an off-white background:
+    mainContext.fillStyle = '#EEE';
+    mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	const {x, y} = state.monkey;
-	const offset = {down: 0, right: 18, up: 36, left: 54}[state.monkey.direction];
-	mainContext.drawImage(monkeyImage,
-		offset, 0, 18, 26,
-		x, y, 18, 26
-	);
+    const frame = getMonkeyFrame(state.monkey);
+    let {x, y} = state.monkey;
+    // The standing frames and walking frames are slightly out of alignment,
+    // so offset the standing frames by 1 pixel to smooth switching between the two.
+    if (!state.monkey.isWalking) {
+        x += 1;
+        y += 1;
+    }
+    drawFrame(mainContext, frame, {...frame, x, y});
 }
 
 // Start update loop:
@@ -76,11 +120,11 @@ update();
 setInterval(update, FRAME_LENGTH);
 
 function renderLoop() {
-	try {
-		render();
-		window.requestAnimationFrame(renderLoop);
-	} catch (error: unknown) {
-		console.error(error);
-	}
+    try {
+        render();
+        window.requestAnimationFrame(renderLoop);
+    } catch (error: unknown) {
+        console.error(error);
+    }
 }
 renderLoop();
