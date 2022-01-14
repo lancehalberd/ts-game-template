f2a704ab21b8d76e9af95a8b85c2dbe882667fab
Chris Brewer <chris.brewer@adroll.com>
2022-01-14 12:03:18 -0600

    Fix whitespace.

--- app/client.ts
+++ app/client.ts
@@ -15,13 +15,13 @@ type Direction = 'up' | 'down' | 'left' | 'right';
 
 // Object to store the game state on.
 const state = {
-	time: 0,
-	lastRender: -1,
-	monkey: {
-		x: CANVAS_WIDTH / 2,
-		y: CANVAS_HEIGHT / 2,
-		direction: 'down' as Direction,
-	}
+    time: 0,
+    lastRender: -1,
+    monkey: {
+        x: CANVAS_WIDTH / 2,
+        y: CANVAS_HEIGHT / 2,
+        direction: 'down' as Direction,
+    }
 };
 
 // 18x26, 4 frames: down - right - up - left
@@ -29,46 +29,46 @@ const monkeyImage = requireImage('gfx/monkey/facing.png');
 
 // Update function advances the state of the game every FRAME_LENGTH milliseconds.
 function update() {
-	state.time += FRAME_LENGTH;
-	if (isKeyboardKeyDown(KEY.UP)) {
-		state.monkey.y--;
-		state.monkey.direction = 'up';
-	}
-	if (isKeyboardKeyDown(KEY.DOWN)) {
-		state.monkey.y++;
-		state.monkey.direction = 'down';
-	}
-	if (isKeyboardKeyDown(KEY.LEFT)) {
-		state.monkey.x--;
-		state.monkey.direction = 'left';
-	}
-	if (isKeyboardKeyDown(KEY.RIGHT)) {
-		state.monkey.x++;
-		state.monkey.direction = 'right';
-	}
+    state.time += FRAME_LENGTH;
+    if (isKeyboardKeyDown(KEY.UP)) {
+        state.monkey.y--;
+        state.monkey.direction = 'up';
+    }
+    if (isKeyboardKeyDown(KEY.DOWN)) {
+        state.monkey.y++;
+        state.monkey.direction = 'down';
+    }
+    if (isKeyboardKeyDown(KEY.LEFT)) {
+        state.monkey.x--;
+        state.monkey.direction = 'left';
+    }
+    if (isKeyboardKeyDown(KEY.RIGHT)) {
+        state.monkey.x++;
+        state.monkey.direction = 'right';
+    }
 }
 
 // Render will display the current state of the game.
 function render() {
-	if (!areAllImagesLoaded()) {
-		return;
-	}
-	// Only render if the state has been updated since the last render.
-	if (state.lastRender >= state.time) {
-		return;
-	}
-	state.lastRender = state.time;
+    if (!areAllImagesLoaded()) {
+        return;
+    }
+    // Only render if the state has been updated since the last render.
+    if (state.lastRender >= state.time) {
+        return;
+    }
+    state.lastRender = state.time;
 
-	// Draw an off-white background:
-	mainContext.fillStyle = '#EEE';
-	mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+    // Draw an off-white background:
+    mainContext.fillStyle = '#EEE';
+    mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	const {x, y} = state.monkey;
-	const offset = {down: 0, right: 18, up: 36, left: 54}[state.monkey.direction];
-	mainContext.drawImage(monkeyImage,
-		offset, 0, 18, 26,
-		x, y, 18, 26
-	);
+    const {x, y} = state.monkey;
+    const offset = {down: 0, right: 18, up: 36, left: 54}[state.monkey.direction];
+    mainContext.drawImage(monkeyImage,
+        offset, 0, 18, 26,
+        x, y, 18, 26
+    );
 }
 
 // Start update loop:
@@ -76,11 +76,11 @@ update();
 setInterval(update, FRAME_LENGTH);
 
 function renderLoop() {
-	try {
-		render();
-		window.requestAnimationFrame(renderLoop);
-	} catch (error: unknown) {
-		console.error(error);
-	}
+    try {
+        render();
+        window.requestAnimationFrame(renderLoop);
+    } catch (error: unknown) {
+        console.error(error);
+    }
 }
 renderLoop();
