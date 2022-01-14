c6bc8e88c04cfabf4d425fd97fe0af1ca8f2db83
Chris Brewer <chris.brewer@adroll.com>
2022-01-13 21:25:04 -0600

    Draw a monkey facing the direction it is moving.

--- app/client.ts
+++ app/client.ts
@@ -1,4 +1,5 @@
 import { CANVAS_WIDTH, CANVAS_HEIGHT, FRAME_LENGTH } from 'app/gameConstants';
+import { areAllImagesLoaded, requireImage } from 'app/utils/images';
 import { bindKeyboardListeners,isKeyboardKeyDown, KEY } from 'app/utils/keyboard';
 
 
@@ -10,35 +11,48 @@ mainContext.imageSmoothingEnabled = false;
 // Bind keyboard listeners to keep track of which keys are pressed.
 bindKeyboardListeners();
 
+type Direction = 'up' | 'down' | 'left' | 'right';
+
 // Object to store the game state on.
 const state = {
 	time: 0,
 	lastRender: -1,
-	square: {
+	monkey: {
 		x: CANVAS_WIDTH / 2,
 		y: CANVAS_HEIGHT / 2,
+		direction: 'down' as Direction,
 	}
 };
+
+// 18x26, 4 frames: down - right - up - left
+const monkeyImage = requireImage('gfx/monkey/facing.png');
 
 // Update function advances the state of the game every FRAME_LENGTH milliseconds.
 function update() {
 	state.time += FRAME_LENGTH;
 	if (isKeyboardKeyDown(KEY.UP)) {
-		state.square.y--;
+		state.monkey.y--;
+		state.monkey.direction = 'up';
 	}
 	if (isKeyboardKeyDown(KEY.DOWN)) {
-		state.square.y++;
+		state.monkey.y++;
+		state.monkey.direction = 'down';
 	}
 	if (isKeyboardKeyDown(KEY.LEFT)) {
-		state.square.x--;
+		state.monkey.x--;
+		state.monkey.direction = 'left';
 	}
 	if (isKeyboardKeyDown(KEY.RIGHT)) {
-		state.square.x++;
+		state.monkey.x++;
+		state.monkey.direction = 'right';
 	}
 }
 
 // Render will display the current state of the game.
 function render() {
+	if (!areAllImagesLoaded()) {
+		return;
+	}
 	// Only render if the state has been updated since the last render.
 	if (state.lastRender >= state.time) {
 		return;
@@ -49,11 +63,12 @@ function render() {
 	mainContext.fillStyle = '#EEE';
 	mainContext.fillRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
 
-	// Draw a green square to the screen that oscillates back and forth over time.
-	mainContext.fillStyle = 'green';
-	const size = 50;
-	const {x, y} = state.square;
-	mainContext.fillRect(x, y, size, size);
+	const {x, y} = state.monkey;
+	const offset = {down: 0, right: 18, up: 36, left: 54}[state.monkey.direction];
+	mainContext.drawImage(monkeyImage,
+		offset, 0, 18, 26,
+		x, y, 18, 26
+	);
 }
 
 // Start update loop:
