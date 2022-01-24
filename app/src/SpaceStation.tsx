51643c00e7946b993211f462399a9141a652e4c5
Matt Burch <matthew.burch@adroll.com>
2022-01-24 14:28:26 -0800

    UI: started defining font family, thinking about Card component

--- app/src/SpaceStation.tsx
+++ app/src/SpaceStation.tsx
@@ -4,11 +4,16 @@ import GameContext from './context';
 const SpaceStation = () => {
     const state = React.useContext(GameContext);
 
-    console.log('state: ', state);
-
     return (
         <div className="space-station">
+            <h1>Eve Offline</h1>
             <h3>You're at the Station.</h3>
+
+            <div className="card">
+                <p>Credits: {state.credits}</p>
+                <p>Credit Limit: {state.creditLimit}</p>
+                <p>Current Ship: {state.currentShip}</p>
+            </div>
         </div>
     );
 };
