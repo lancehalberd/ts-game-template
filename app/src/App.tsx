8a4c134536d7d56b1ad12b188606657fe7e22451
Matt Burch <matthew.burch@adroll.com>
2022-01-24 13:26:51 -0800

    UI: continued stubbing of basic components

---
+++ app/src/App.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+const App = () => {
+    return (
+        <div className="app">
+            <h3>Welcome from React!!</h3>
+            <p>Open console to play.</p>
+            <p>Call functions on `gameApi` to take actions.</p>
+            <p>UI + other utilities coming soon!</p>
+        </div>
+    )
+}
+
+export default App;
---
+++ app/src/SpaceStation.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+const SpaceStation = () => {
+    return (
+        <div className='space-station'>
+            <h3>You're at the Station.</h3>
+        </div>
+    )
+}
+
+export default SpaceStation;
