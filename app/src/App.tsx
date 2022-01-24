55c1ac6bf46de767bca7012ab8b684f168fc80f4
Matt Burch <matthew.burch@adroll.com>
2022-01-24 14:07:35 -0800

    UI: basic SASS implementation

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,8 @@ import GameContext from './context';
 import { defaultState } from '../content';
 import SpaceStation from './SpaceStation';
 
+import 'app/styles/App.scss';
+
 const App = () => {
     return (
         <GameContext.Provider value={defaultState}>
--- app/src/SpaceStation.tsx
+++ app/src/SpaceStation.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
+import GameContext from './context';
 
 const SpaceStation = () => {
+    const state = React.useContext(GameContext);
+
+    console.log('state: ', state);
+
     return (
-        <div className='space-station'>
+        <div className="space-station">
             <h3>You're at the Station.</h3>
         </div>
-    )
-}
+    );
+};
 
 export default SpaceStation;
