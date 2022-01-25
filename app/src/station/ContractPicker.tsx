02b2e163850344ad652a0d283159465275533f9d
Matt Burch <matthew.burch@adroll.com>
2022-01-25 12:36:25 -0800

    UI: Fleshing out ContractPicker

--- app/src/station/ContractPicker.tsx
+++ app/src/station/ContractPicker.tsx
@@ -1,26 +1,52 @@
 import {
+    Button,
     List,
     ListItem,
     ListItemButton,
     ListItemIcon,
     ListItemText,
+    Paper,
+    Stack,
 } from '@mui/material';
 import ArticleIcon from '@mui/icons-material/Article';
 
 import * as React from 'react';
 import { GameContext } from '../App';
 
+const ContractDetailItem = ({
+    label,
+    value,
+}: {
+    label: string;
+    value: string | number;
+}): JSX.Element => {
+    return (
+        <Paper>
+            {label}: {value}
+        </Paper>
+    );
+};
+
 const ContractPicker = () => {
     const { gameState } = React.useContext(GameContext);
+    const [selectedContract, setSelectedContract] = React.useState<Contract>();
+
+    const handleContractClick = (contract: Contract) => {
+        setSelectedContract(contract);
+    };
 
     return (
-        <div className="contract-picker">
+        <div className="contract-picker" style={{ display: 'flex' }}>
             <div className="contract-list">
                 <List>
                     {gameState.station.availableContracts.map((contract) => {
                         return (
-                            <ListItem disablePadding>
-                                <ListItemButton>
+                            <ListItem disablePadding key={contract.id}>
+                                <ListItemButton
+                                    onClick={() =>
+                                        handleContractClick(contract)
+                                    }
+                                >
                                     <ListItemIcon>
                                         <ArticleIcon />
                                     </ListItemIcon>
@@ -33,6 +59,39 @@ const ContractPicker = () => {
                     })}
                 </List>
             </div>
+            {selectedContract && (
+                <div className="contract-details">
+                    <Stack spacing={2}>
+                        <ContractDetailItem
+                            label="Cost"
+                            value={selectedContract.cost}
+                        />
+                        <ContractDetailItem
+                            label="Cargo"
+                            value={selectedContract.cargo.join(',')}
+                        />
+                        <ContractDetailItem
+                            label="Cargo Space"
+                            value={selectedContract.cargoSpace}
+                        />
+                        <ContractDetailItem
+                            label="Distance"
+                            value={selectedContract.distance}
+                        />
+                        <ContractDetailItem
+                            label="Grid Points"
+                            value={selectedContract.grid.length}
+                        />
+                        <Button
+                            variant="contained"
+                            color="success"
+                            size="large"
+                        >
+                            Select This Contract
+                        </Button>
+                    </Stack>
+                </div>
+            )}
         </div>
     );
 };
