4e1bddbffde8c2600a9e1a2cb06796b56693ea39
Chris Brewer <chris.brewer@adroll.com>
2022-01-27 14:33:40 -0600

    Small trip preview improvements.

--- app/src/station/PreviewTrip.tsx
+++ app/src/station/PreviewTrip.tsx
@@ -3,19 +3,19 @@ import { getTotalShipFuel } from 'app/state';
 import * as React from 'react';
 import { GameContext } from '../App';
 
+const MIN_FUEL_BURN = 10;
+
 const FuelSlider = ({
     onChange,
     maxAmount,
-    defaultAmount = 20,
+    amount,
 }: {
     onChange: (units: number) => void;
     maxAmount: number;
-    defaultAmount?: number;
+    amount: number;
 }) => {
-    const [fuelUnits, setFuelUnits] = React.useState<number>(defaultAmount);
     const handleChange = (event: Event, newValue: number | number[]) => {
         const newUnits = newValue as number;
-        setFuelUnits(newUnits);
         onChange(newUnits);
     };
 
@@ -23,9 +23,9 @@ const FuelSlider = ({
         <div className="preview-fuel-slider">
             <Slider
                 aria-label="Fuel Units"
-                value={fuelUnits}
+                value={amount}
                 max={maxAmount}
-                min={2}
+                min={MIN_FUEL_BURN}
                 valueLabelDisplay="auto"
                 onChange={handleChange}
             />
@@ -33,54 +33,68 @@ const FuelSlider = ({
     );
 };
 
+
 const PreviewTrip = () => {
     const { gameState, gameApi, refreshGameState } =
         React.useContext(GameContext);
-    const [fuelBurnUnits, setFuelBurnUnits] = React.useState(0);
-    const contract = gameState.currentContract;
     const ship = gameState.station.ships[0];
+    const shipFuelUnits = ship ? getTotalShipFuel(ship) : 0;
+    const [fuelBurnUnits, setFuelBurnUnits] = React.useState(Math.max(MIN_FUEL_BURN, Math.floor(shipFuelUnits / 2)));
+    const contract = gameState.currentContract;
     const diggingTool = ship?.cargo.find(
         (cargoItem) => cargoItem.type === 'tool'
     );
-    const shipFuelUnits = ship ? getTotalShipFuel(ship) : 0;
 
-    if (!contract || !ship || !diggingTool || shipFuelUnits === 0) {
+    const requirements: string[] = [];
+    if (!contract) {
+        requirements.push('Purchase a Contract');
+    }
+    if (!ship) {
+        requirements.push('Purchase or rent a ship');
+    }
+    if (shipFuelUnits < MIN_FUEL_BURN) {
+        requirements.push(`Purchase at least ${MIN_FUEL_BURN}L of Fuel for your Ship`);
+    }
+    if (!diggingTool) {
+        requirements.push('Purchase a Digging Tool');
+    }
+    if (requirements.length) {
         return (
             <div style={{ padding: '40px' }}>
-                <h3>Before you can Preview</h3>
-                <p>You need to:</p>
+                <h3>Before you can depart</h3>
+                <p>You still need to:</p>
                 <ul>
-                    <li>Purchase a Contract</li>
-                    <li>Rent a Ship</li>
-                    <li>Purchase Fuel for your Ship</li>
-                    <li>Purchase a Digging Tool</li>
+                    { requirements.map(text => (<li key={text}>{text}</li>))}
                 </ul>
             </div>
         );
     }
 
-    const actualFuelBurnUnits =
-        fuelBurnUnits > 0 ? fuelBurnUnits : Math.floor(shipFuelUnits / 2);
-    const simulateApi = gameApi.simulate();
-    const startTime = simulateApi!.state!.time;
-    simulateApi.travelToContract(ship.shipType, actualFuelBurnUnits, {
-        ignoreLongTravelTime: true,
-    });
-    const timeToTravel = Math.floor(simulateApi!.state!.time - startTime);
+    let timeToTravel: string;
+    try {
+        const simulateApi = gameApi.simulate();
+        const startTime = simulateApi!.state!.time;
+        simulateApi.travelToContract(ship.shipType, fuelBurnUnits, {
+            ignoreLongTravelTime: true,
+        });
+        timeToTravel = `${Math.floor(simulateApi!.state!.time - startTime)} days`;
+    } catch {
+        timeToTravel = 'Error, try burning more fuel';
+    }
 
     const handleFuelChange = (units: number) => {
         setFuelBurnUnits(units);
     };
 
     const handleEmbarkClick = () => {
-        gameApi.travelToContract(ship.shipType, actualFuelBurnUnits);
+        gameApi.travelToContract(ship.shipType, fuelBurnUnits);
         refreshGameState();
     };
 
     return (
         <div className="preview-trip">
             <h3>Distance to Asteroid:</h3>
-            {contract.distance} meters
+            {contract!.distance} meters
             <h3>Fuel to Burn:</h3>
             <Stack
                 spacing={2}
@@ -88,16 +102,16 @@ const PreviewTrip = () => {
                 sx={{ mb: 1 }}
                 alignItems="center"
             >
-                <h3 style={{ color: 'green' }}>2</h3>
+                <h3 style={{ color: 'green' }}>{MIN_FUEL_BURN}</h3>
                 <FuelSlider
                     maxAmount={shipFuelUnits}
                     onChange={handleFuelChange}
-                    defaultAmount={Math.floor(shipFuelUnits / 2)}
+                    amount={fuelBurnUnits}
                 />
                 <h3 style={{ color: 'orange' }}>{shipFuelUnits} units</h3>
             </Stack>
             <h3>Time to Arrive:</h3>
-            {timeToTravel} days
+            { timeToTravel }
             <div className="action-buttons">
                 <Button
                     variant="contained"
